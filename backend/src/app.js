49cd5be6cc337e982e3e4913e7d0b490825c4022
IglanCardeal <cmtcardeal@outlook.com>
2019-12-15 13:38:50 -0300

    Refatorando codigo backend.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { join } from "path";
-import dotenv from "dotenv";
 import helmet from "helmet";
 
 import appRoutes from "./routes/app.routes";
 
 import errorHandler from "./middleware/error-handler";
 import multerFileStorage from "./middleware/multer-filestorage";
-
 import cors from "./middleware/cors";
-
-dotenv.config();
 
 const app = express();
 
